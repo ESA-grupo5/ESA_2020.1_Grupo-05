fc7e2459da33c887380354ff61d9e409ccfc7d35
AndersonBarbosaDeFreitas <anderfreitas12@gmail.com>
2020-11-19 17:49:58 -0300

    Created the card control class

---
+++ Code/Flahscard/lib/database/controllers/paperboards_ctr.dart
@@ -0,0 +1,58 @@
+import 'package:Flahscard/database/database_helper.dart';
+import 'package:Flahscard/models/paperboard.dart';
+
+import 'dart:async';
+
+class TopicsCtr {
+  DatabaseHelper con = new DatabaseHelper();
+
+  Future<int> insertPaperboard(Paperboard paperboard) async {
+    var dbClient = await con.db;
+    int res = await dbClient.insert("paperboards", paperboard.toMap());
+    return res;
+  }
+
+  Future<int> updatePaperboard(Paperboard paperboard) async {
+    var db = await con.db;
+    final int result = await db.update(
+      'paperboards',
+      paperboard.toMap(),
+      where: 'id = ?',
+      whereArgs: [paperboard.id],
+    );
+    return result;
+  }
+
+  Future<int> deletePaperboard(int id) async {
+    var db = await con.db;
+    final int result = await db.delete(
+      'paperboards',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    return result;
+  }
+
+  Future<Paperboard> getPaperboard(int id) async {
+    var dbClient = await con.db;
+    var res = await dbClient
+        .rawQuery("SELECT * FROM paperboards WHERE id = '${id.toString()}'");
+
+    if (res.length > 0) {
+      return new Paperboard.fromMap(res.first);
+    }
+    return null;
+  }
+
+  Future<List<Paperboard>> getAllPaperboards(int topicId) async {
+    var dbClient = await con.db;
+    final List<Map<String, dynamic>> result = await dbClient
+        .rawQuery("SELECT * FROM paperboards WHERE topicId = $topicId");
+    final List<Paperboard> paperboards = [];
+    result.forEach((paperboardMap) {
+      paperboards.add(Paperboard.fromMap(paperboardMap));
+    });
+
+    return paperboards;
+  }
+}
