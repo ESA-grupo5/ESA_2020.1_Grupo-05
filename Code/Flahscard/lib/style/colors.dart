7fc388f89bf9b30d9779afad3f31fcb55266c381
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2020-11-26 15:22:07 -0300

    Archive with colors used in app

---
+++ Code/Flahscard/lib/style/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color colorPrimary = Color(0xff622162);
+Color colorPrimaryAccent = Color(0xff4746A3);
+Color colorSecondary = Color(0xffE65870);
+Color colorTertiary = Color(0xffE06A1C);
+Color colorQuartenary = Color(0xff3AD29F);
+
+Color colorTextPrimary = Color(0xff424242);
