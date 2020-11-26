d8d4049c7eae3cba3e2b03ec8102cb13a7e6a0b2
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2020-11-26 15:20:52 -0300

    Profile screen

---
+++ Code/Flahscard/lib/pages/tabs/profilepage.dart
@@ -0,0 +1,128 @@
+import 'package:Flahscard/constants.dart';
+import 'package:Flahscard/database/controllers/users_ctr.dart';
+import 'package:Flahscard/functions/account.dart';
+import 'package:Flahscard/models/user.dart';
+import 'package:Flahscard/pages/welcome_page.dart';
+import 'package:Flahscard/style/colors.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Profilepage extends StatefulWidget {
+  @override
+  _ProfilepageState createState() => _ProfilepageState();
+}
+
+class _ProfilepageState extends State<Profilepage> {
+  User _user = User();
+  LoginCtr _controller = LoginCtr();
+
+  @override
+  initState() {
+    super.initState();
+    getUser();
+  }
+
+  getUser() async {
+    User user =
+        await _controller.getUser(userIdConstant).then((value) => value);
+    setState(() {
+      _user = user;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Stack(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height / 2.5,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: colorPrimary,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            children: [
+              SizedBox(width: 8),
+              Container(
+                padding: EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.black26,
+                ),
+                child: Center(
+                  child: IconButton(
+                    iconSize: 25,
+                    padding: EdgeInsets.zero,
+                    icon: Icon(
+                      EvaIcons.edit,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 57.5,
+                    child: Icon(
+                      EvaIcons.imageOutline,
+                      size: 57.5,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    '${_user.name}',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    '${_user.email}',
+                    style: TextStyle(
+                      color: Colors.white30,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.black26,
+                ),
+                child: Center(
+                  child: IconButton(
+                    iconSize: 25,
+                    padding: EdgeInsets.zero,
+                    icon: Icon(
+                      EvaIcons.logOut,
+                      color: Colors.white,
+                    ),
+                    onPressed: () async {
+                      await logOut().then((value) {
+                        Navigator.of(context, rootNavigator: true)
+                            .pushReplacement(CupertinoPageRoute(
+                                builder: (context) => WelcomePage()));
+                      });
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(width: 8),
+            ],
+          ),
+        )
+      ],
+    ));
+  }
+}
